1d62ee1062ccd69a3234a672987e24debca1af18
montel26 <montelwood202@gmail.com>
2024-10-02 22:37:17 +0200

    landing page in progress ,other sections soon to come.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
