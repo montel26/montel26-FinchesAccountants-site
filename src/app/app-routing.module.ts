f2b93e7e536a8677ded71bd4a3fccc81713298c5
montel26 <montelwood202@gmail.com>
2024-10-04 07:11:43 +0200

    ix fonts but overrall good

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {  
     path:"",
-    component: BodyComponent
+    component: AboutComponent
   }
 ];
 
