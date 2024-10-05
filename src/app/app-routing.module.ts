ce476dee69a591b55773cf1f08ffc46d186f9a75
montel26 <montelwood202@gmail.com>
2024-10-05 14:26:12 +0200

    wee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   {  
     path:"",
-    component: BodyComponent
+    component: FooterComponent
   }
 ];
 
